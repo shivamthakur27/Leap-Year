a3d25c6c43e8b1b422b1250cb88dc0f66613a844
shivamthakur27 <shvm1995@gmail.com>
2020-05-14 12:04:20 +0530

    Adding check for year divisible by 4 & 400.

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,7 +1,12 @@
 public class LeapYear
 {
+    private final int year;
     LeapYear(int year)
     {
-
+        this.year = year;
+    }
+    public Boolean isLeapYear()
+    {
+        return year % 400 == 0 || year%4 ==0;
     }
 }
--- src/test/java/LeapYearTest.java
+++ src/test/java/LeapYearTest.java
@@ -1,10 +1,21 @@
 import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class LeapYearTest
 {
     @Test
-    public void test()
+    public void test_year_divisible_by_four()
     {
-
+        LeapYear leapYear = new LeapYear(2008);
+        assertThat(leapYear.isLeapYear(), is(true));
     }
+
+    @Test
+    public void test_year_not_divisible_by_four()
+    {
+        LeapYear leapYear = new LeapYear(1997);
+        assertThat(leapYear.isLeapYear(), is(false));
+    }
+
 }

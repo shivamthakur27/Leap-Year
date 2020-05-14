e8c0efa4a4fee078580b511963eff45d4ac02fc2
shivamthakur27 <shvm1995@gmail.com>
2020-05-14 12:31:15 +0530

    Code refactoring.

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,10 +1,12 @@
 public class LeapYear
 {
     private final int year;
+
     LeapYear(int year)
     {
         this.year = year;
     }
+
     public Boolean isLeapYear()
     {
         return (((year % 4 ==0) && !(year % 100 == 0)) || (year % 400 == 0 )) ;
--- src/test/java/LeapYearTest.java
+++ src/test/java/LeapYearTest.java
@@ -7,37 +7,35 @@ public class LeapYearTest
     @Test
     public void test_year_divisible_by_4()                                  //check year divisible by 400 is a Leap Year
     {
-        LeapYear leapYear = new LeapYear(2008);
-        assertThat(leapYear.isLeapYear(), is(true));
+        assertThat(leapYear(2008), is(true));
     }
 
     @Test
     public void test_year_not_divisible_by_4()                              //check year not divisible by 4 is not a Leap Year
     {
-        LeapYear leapYear = new LeapYear(2019);
-        assertThat(leapYear.isLeapYear(), is(false));
+        assertThat(leapYear(2019), is(false));
     }
 
     @Test
     public void test_year_divisible_by_400()                                //check all year divisible by 400 is a Leap Year
     {
-        LeapYear leapYear = new LeapYear(2000);
-        assertThat(leapYear.isLeapYear(), is(true));
+        assertThat(leapYear(2000), is(true));
     }
 
     @Test
     public void test_year_not_divisible_by_400()                            //check year not divisible by 400 is a not Leap Year
     {
-        LeapYear leapYear = new LeapYear(2009);
-        assertThat(leapYear.isLeapYear(), is(false));
+        assertThat(leapYear(2009), is(false));
     }
 
     @Test
     public void test_year_divisible_by_100_not_by_400()                     //check all year divisible by 100 but not by 400 are NOT a Leap Year
     {
-        LeapYear leapYear = new LeapYear(1700);
-        assertThat(leapYear.isLeapYear(), is(false));
+        assertThat(leapYear(1700), is(false));
     }
 
-
+    private boolean leapYear(int year)
+    {
+        return new LeapYear(year).isLeapYear();
+    }
 }
